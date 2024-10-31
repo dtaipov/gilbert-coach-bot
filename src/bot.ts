ee201246a8610db32d4366717415aa363994ee8a
dtaipov <dtaipov@gmail.com>
2024-10-31 11:43:24 +0300

    fix imports from constants

--- src/bot.ts
+++ src/bot.ts
@@ -2,8 +2,8 @@ import "dotenv/config";
 import { Bot, webhookCallback, Context } from "grammy";
 import express from "express";
 import { isOperator, getIdStr, OPERATOR_CHAT_IDS } from "./utils";
-import * as answers from "./constants/answers";
-import * as message from "./constants/message";
+import { INTRODUCTION } from "./constants/answers";
+import { USER_INFO_BEGINNING, MESSAGE_FROM_BEGINNING } from "./constants/message";
 
 export type BotContext = Context;
 
@@ -17,8 +17,8 @@ const reply = async (ctx: BotContext) => {
   if (isOperator(ctx.message)) {
     const replyToMessage = ctx.message.reply_to_message;
     if (replyToMessage && replyToMessage.text) {
-      if (replyToMessage.text.startsWith(message.MESSAGE_FROM_BEGINNING)) {
-        const chatIdStr = getIdStr(replyToMessage.text, message.MESSAGE_FROM_BEGINNING);
+      if (replyToMessage.text.startsWith(MESSAGE_FROM_BEGINNING)) {
+        const chatIdStr = getIdStr(replyToMessage.text, MESSAGE_FROM_BEGINNING);
         try {
           await bot.api.sendMessage(Number(chatIdStr), `${ctx.message.text}`);
         } catch (e) {
@@ -34,14 +34,14 @@ const reply = async (ctx: BotContext) => {
     const operatorId = OPERATOR_CHAT_IDS[i];
     await bot.api.sendMessage(
       operatorId,
-      `${message.MESSAGE_FROM_BEGINNING}${ctx.message.from.id}: ${ctx.message.text}`,
+      `${MESSAGE_FROM_BEGINNING}${ctx.message.from.id}: ${ctx.message.text}`,
       {
         reply_markup: {
           inline_keyboard: [
             [
               {
                 text: "Информация о пользователе",
-                callback_data: `${message.USER_INFO_BEGINNING}:${ctx.message.from.id}`,
+                callback_data: `${USER_INFO_BEGINNING}:${ctx.message.from.id}`,
               },
             ],
           ],
@@ -64,7 +64,7 @@ bot.on("message:file", async (ctx) => {
     ctx.reply("Отправка файлов боту не поддерживается");
   }
 });
-bot.command("start", (ctx) => ctx.reply(answers.INTRODUCTION, { reply_markup: keyboardCommon }));
+bot.command("start", (ctx) => ctx.reply(INTRODUCTION, { reply_markup: keyboardCommon }));
 bot.on("message:text", reply);
 
 if (process.env.NODE_ENV === "production") {
